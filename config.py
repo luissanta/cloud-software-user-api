7e39444c1485ee796ed41b163c78ffab5edd00ca
felipe <lf.garciar1@uniandes.edu.co>
2023-04-15 03:28:36 -0500

    complete postgres

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     STATIC_FOLDER = "views/static/"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///user.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:admin@localhost/user'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
     JWT_SECRET_KEY = 'SFDJKGKJFD7SG987FDS?9889'
