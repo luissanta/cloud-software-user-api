88d2337cdaa63bdf24c8da797b41659a4bc62397
felipe <lf.garciar1@uniandes.edu.co>
2023-04-15 09:38:34 -0500

    env prod

--- config.py
+++ config.py
@@ -6,7 +6,13 @@ load_dotenv()
 
 class Config:
     STATIC_FOLDER = "views/static/"
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:admin@localhost/user'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:admin@localhost/user'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + \
+                              os.environ.get('PGSQL_USER') + ':' + \
+                              os.environ.get('PGSQL_PASSWORD') + '@' + \
+                              os.environ.get('PGSQL_HOST') + ':' + \
+                              os.environ.get('PGSQL_PORT') + '/' + \
+                              os.environ.get('PGSQL_DATABASE')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
     JWT_SECRET_KEY = 'SFDJKGKJFD7SG987FDS?9889'
