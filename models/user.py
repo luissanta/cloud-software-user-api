daf5f02226e07435b3399d787b5ab010ce04fbf4
Luis <luisfernandosantaospina@gmail.com>
2023-04-24 21:10:02 -0500

    Add timestamp

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from models.database import database
+from datetime import datetime
 
 
 class User(database.Model):
@@ -9,6 +10,8 @@ class User(database.Model):
     username = database.Column(database.String(100), unique=True)
     password = database.Column(database.String(100))
     email = database.Column(database.String(100))
+    created_at = database.Column(database.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = database.Column(database.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
 
 class UserSchema(SQLAlchemyAutoSchema):
